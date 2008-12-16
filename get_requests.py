cc3bf475273bda33bac8c0af9ef53b6d467be237
Leigh Honeywell <hypatia@keller.(none)>
2008-12-15 23:14:40 -0500

    initial backend setup

---
+++ get_requests.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+### collect all the requests from the infotrace server, and process them
+
+
+import mechanize
+import os
+import sys
+import popen2
+
+BASE_URL = "http://infotrace.citizenlab.org/requests.txt"
+br = mechanize.Browser()
+data = br.open(BASE_URL).get_data()
+print data
+
+for line in data.split("\n"):
+    if line == "": pass
+    elif line[0] =="#": pass
+    else:
+        (id,owner_id,name,type,node,destination) = line.split(",")
+        proc = popen2.Popen3("ssh utoronto_infotrace@" + node + " sudo " + type + " " + destination)
+        fromchild = proc.fromchild.readline()
+        sys.stdout.write(fromchild)
+        while fromchild:
+            fromchild = proc.fromchild.readline()
+            sys.stdout.write(fromchild) 
+        print fromchild
